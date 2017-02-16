fbf2936848bb235efb17b69e0120b887a53d85ae
rlopez133 <rlopez@redhat.com>
2017-02-16 14:41:59 +0000

    creating a floating ip on the ext-net, attach a floating ip to an avail server

---
+++ paper_scripts/create-floating-ip.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+DOMAIN=${DOMAIN:-control.ocp3.e2e.bos.redhat.com}
+MASTERS="master-0 master-1 master-2"
+BASTION="bastion-rl"
+INFRA_NODES="infra-node-0 infra-node-1"
+APP_NODES="app-node-0 app-node-1 app-node-2"
+ALL_NODES="$INFRA_NODES $APP_NODES"
+ALL_HOSTS="$BASTION $MASTERS $ALL_NODES"
+for HOST in $BASTION $MASTERS $INFRA_NODES
+do
+  openstack floating ip create ext-net
+  FLOATING_IP=$(openstack floating ip list | awk "/None/ { print \$4 }")
+#  echo "host: ${HOST} , ${DOMAIN} , ${FLOATING_IP}"
+  openstack server add floating ip ${HOST}.${DOMAIN} ${FLOATING_IP}
+done
