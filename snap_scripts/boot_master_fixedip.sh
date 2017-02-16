7ec7cceb9525443a2abed4d34c75daf322b3b69e
rlopez133 <rlopez@redhat.com>
2017-02-16 16:08:42 +0000

    boot 3 master nodes with fixed control and tenant networks

---
+++ snap_scripts/boot_master_fixedip.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+DOMAIN=${DOMAIN:-control.ocp3.e2e.bos.redhat.com}
+netid1=$(openstack network list | awk "/control-network/ { print \$2 }")
+netid2=$(openstack network list | awk "/tenant-network/ { print \$2 }")
+for HOSTNUM in 0 ; do
+  openstack server create --flavor m1.medium --image master-infra-app-nodes-2017-02-16-snap --key-name rlopez \
+  --nic net-id=$netid1,v4-fixed-ip="172.18.10.15" \
+  --nic net-id=$netid2,v4-fixed-ip="172.18.20.16" \
+  --user-data=user-data/master-${HOSTNUM}.yaml \
+  master-${HOSTNUM}.${DOMAIN}
+done
+
+for HOSTNUM in 1 ; do
+  openstack server create --flavor m1.medium --image master-infra-app-nodes-2017-02-16-snap --key-name rlopez \
+  --nic net-id=$netid1,v4-fixed-ip="172.18.10.17" \
+  --nic net-id=$netid2,v4-fixed-ip="172.18.20.20" \
+  --user-data=user-data/master-${HOSTNUM}.yaml \
+  master-${HOSTNUM}.${DOMAIN}
+done
+
+for HOSTNUM in 2 ; do
+  openstack server create --flavor m1.medium --image master-infra-app-nodes-2017-02-16-snap --key-name rlopez \
+  --nic net-id=$netid1,v4-fixed-ip="172.18.10.13" \
+  --nic net-id=$netid2,v4-fixed-ip="172.18.20.6" \
+  --user-data=user-data/master-${HOSTNUM}.yaml \
+  master-${HOSTNUM}.${DOMAIN}
+done
+
