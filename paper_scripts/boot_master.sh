1576a758adb78383afde19290a247d1e14907792
rlopez133 <rlopez@redhat.com>
2017-02-16 02:10:42 +0000

    moving all scripts into paper_scripts dir

---
+++ paper_scripts/boot_master.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+DOMAIN=${DOMAIN:-control.ocp3.e2e.bos.redhat.com}
+netid1=$(openstack network list | awk "/control-network/ { print \$2 }")
+netid2=$(openstack network list | awk "/tenant-network/ { print \$2 }")
+for HOSTNUM in 0 1 2 ; do
+  openstack server create --flavor m1.medium --image master-infra-app-nodes-2017-02-16-snap --key-name rlopez \
+  --nic net-id=$netid1 --nic net-id=$netid2 \
+  --security-group master-sg --user-data=user-data/master-${HOSTNUM}.yaml \
+  master-${HOSTNUM}.${DOMAIN}
+done
+
