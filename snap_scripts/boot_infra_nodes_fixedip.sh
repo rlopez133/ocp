b4da408dab3dd24cd145e30bd0ea11246a6ff150
rlopez133 <rlopez@redhat.com>
2017-02-16 20:44:30 +0000

    fixed tenant ip issue

--- snap_scripts/boot_infra_nodes_fixedip.sh
+++ snap_scripts/boot_infra_nodes_fixedip.sh
@@ -6,7 +6,7 @@ for HOSTNUM in 0 ; do
   VOLUMEID=$(cinder list | awk "/infra-node-${HOSTNUM}-docker/ { print \$2 }")
   openstack server create --flavor m1.medium --image master-infra-app-nodes-2017-02-16-snap --key-name rlopez \
   --nic net-id=$netid1,v4-fixed-ip="172.18.10.21" \
-  --nic net-id=$netid2,v4-fixed-ip="172.18.20.3" \
+  --nic net-id=$netid2,v4-fixed-ip="172.18.20.30" \
   --security-group infra-node-sg --user-data=user-data/infra-node-${HOSTNUM}.yaml \
   --block-device-mapping vdb=${VOLUMEID} \
   infra-node-${HOSTNUM}.${DOMAIN}
@@ -15,7 +15,7 @@ for HOSTNUM in 1 ; do
   VOLUMEID=$(cinder list | awk "/infra-node-${HOSTNUM}-docker/ { print \$2 }")
   openstack server create --flavor m1.medium --image master-infra-app-nodes-2017-02-16-snap --key-name rlopez \
   --nic net-id=$netid1,v4-fixed-ip="172.18.10.22" \
-  --nic net-id=$netid2,v4-fixed-ip="172.18.20.4" \
+  --nic net-id=$netid2,v4-fixed-ip="172.18.20.31" \
   --security-group infra-node-sg --user-data=user-data/infra-node-${HOSTNUM}.yaml \
   --block-device-mapping vdb=${VOLUMEID} \
   infra-node-${HOSTNUM}.${DOMAIN}
