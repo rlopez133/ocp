bf8766deb51d1b27e449f4b662fbb3fe8d03a2d5
Roger Lopez <rlopez@redhat.com>
2017-03-23 20:00:52 +0000

    delete servers except bastion, need to update

--- snap_scripts/delete_servers.sh
+++ snap_scripts/delete_servers.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for x in $(openstack server list | awk '/com/ {print $2}')
+for x in $(openstack server list | awk '/com/ {print $2}'| grep -v 898a5b03-f8a1-4b89-9d14-3bb824bcc423)
 do
         openstack server delete $x
 done
