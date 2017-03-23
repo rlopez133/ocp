b5df21521e7982d1409394292e17c45a3f7e157c
Roger Lopez <rlopez@redhat.com>
2017-03-23 19:57:56 +0000

    fixed way of adding ips for my ocp env

--- snap_scripts/add_fixed_floatingip.sh
+++ snap_scripts/add_fixed_floatingip.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 DOMAIN=${DOMAIN:-control.ocp3.e2e.bos.redhat.com}
 
-openstack server add floating ip bastion-rl.${DOMAIN} 10.19.5.68
+#openstack server add floating ip bastion-rl.${DOMAIN} 10.19.5.68
 openstack server add floating ip master-0.${DOMAIN} 10.19.5.76
 openstack server add floating ip master-1.${DOMAIN} 10.19.5.82
 openstack server add floating ip master-2.${DOMAIN} 10.19.5.84
