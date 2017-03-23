a76236632219833b0a888f2093b5e90f8dc74f44
Roger Lopez <rlopez@redhat.com>
2017-03-23 20:06:24 +0000

    static add of my boot_bastion with ip

--- paper_scripts/boot_bastion.sh
+++ paper_scripts/boot_bastion.sh
@@ -4,7 +4,7 @@ netid1=$(openstack network list | awk "/control-network/ { print \$2 }")
 netid2=$(openstack network list | awk "/tenant-network/ { print \$2 }")
 openstack server create --flavor m1.small --image rhel7 \
 --key-name rlopez \
---nic net-id=$netid1 \
---nic net-id=$netid2 \
+--nic net-id=$netid1,v4-fixed-ip="172.18.10.7" \
+--nic net-id=$netid2,v4-fixed-ip="172.18.20.10" \
 --security-group bastion-sg --user-data=user-data/bastion.yaml \
 bastion-rl.${DOMAIN}
