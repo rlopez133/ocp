34fe86aa612017ad9330a57ba39f2127cb219c19
Roger Lopez <rlopez@redhat.com>
2017-07-14 10:44:43 -0400

    added security grp scripts

--- paper_scripts/delete-floating.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for y in $(openstack floating ip list | awk "/None/ { print \$4 }")
-do
-        openstack floating ip delete $y
-done
-
--- paper_scripts/latest-floating-ip.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-openstack server add floating ip infra-node-0.control.osp10.e2e.bos.redhat.com 10.19.114.154
-openstack server add floating ip infra-node-1.control.osp10.e2e.bos.redhat.com 10.19.114.150
-openstack server add floating ip master-0.control.osp10.e2e.bos.redhat.com 10.19.114.142
-openstack server add floating ip master-1.control.osp10.e2e.bos.redhat.com 10.19.114.153
-openstack server add floating ip master-2.control.osp10.e2e.bos.redhat.com 10.19.114.155
-
